6fadeb4d38c17c5192839647475ded681a583b9c
Adrian Messado <adrian.messado@gmail.com>
2019-05-26 22:00:21 -0700

    create string array to be used with for loop, use for loop to display array items to console

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,9 +1,13 @@
 public class GroceryList {
     public static void main(String[] args) {
-        String[] myGroceryList = {"ice cream", "hot sauce", "pickles", "bananas", "cereal", "rice", "yogurt"};
-
-        for (String groceryItem : myGroceryList) {
+        String[] myGroceryListForEach= {"ice cream", "hot sauce", "pickles", "bananas", "cereal", "rice", "yogurt"};
+        String[] myGroceryListFor = { "eggs", "milk", "bread", "bananas", "cereal", "rice"};
+        for (String groceryItem : myGroceryListForEach) {
             System.out.println(groceryItem);
+        }
+
+        for (Integer index = 0 ; index < myGroceryListFor.length ; index++) {
+            System.out.println(myGroceryListFor[index]);
         }
     }
 }
