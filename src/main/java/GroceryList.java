772eb248d96a7160b4af10446e099ff6c770d725
Adrian Messado <adrian.messado@gmail.com>
2019-05-26 22:43:35 -0700

    print out one grocery list using forEach loop and one using for loop

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,13 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class GroceryList {
     public static void main(String[] args) {
-        String[] myGroceryListForEach= {"ice cream", "hot sauce", "pickles", "bananas", "cereal", "rice", "yogurt"};
-        String[] myGroceryListFor = { "eggs", "milk", "bread", "bananas", "cereal", "rice"};
-        for (String groceryItem : myGroceryListForEach) {
-            System.out.println(groceryItem);
-        }
 
-        for (Integer index = 0 ; index < myGroceryListFor.length ; index++) {
-            System.out.println(myGroceryListFor[index]);
-        }
+            String[] myGroceryListForEach = {"ice cream", "hot sauce", "pickles", "bananas", "cereal", "rice", "yogurt"};
+            String[] myGroceryListFor = {"eggs", "milk", "bread", "bananas", "cereal", "rice"};
+            for (String groceryItem : myGroceryListForEach) {
+                System.out.println(groceryItem);
+            }
+
+            for (Integer index = 0; index < myGroceryListFor.length; index++) {
+                System.out.println(myGroceryListFor[index]);
+            }
+
     }
 }
