ab87f950cb64f06c07a3ff3165efe1e02660bf1b
Adrian Messado <adrian.messado@gmail.com>
2019-05-26 21:54:39 -0700

    create GroceryList class, create grocerylist array and print out each item in array with for each loop

---
+++ src/main/java/GroceryList.java
@@ -0,0 +1,9 @@
+public class GroceryList {
+    public static void main(String[] args) {
+        String[] myGroceryList = {"ice cream", "hot sauce", "pickles", "bananas", "cereal", "rice", "yogurt"};
+
+        for (String groceryItem : myGroceryList) {
+            System.out.println(groceryItem);
+        }
+    }
+}
